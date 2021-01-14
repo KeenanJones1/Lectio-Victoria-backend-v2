74fc36971709dc5aba9cdf2d3d8793034c562ee5
Keenan Jones <keenanjones210@gmail.com>
2021-01-14 12:20:24 -0600

    creating a book

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -5,9 +5,15 @@ class BookController < ApplicationController
   end
 
   def create
-    book = Book.new(params['book'])
-    byebug
+    @author = params['book']['volumeInfo']['authors'].map { |i| i.to_s }.join(",")
 
+  @title = params['book']['volumeInfo']['title']
+  @published_year = params['book']['volumeInfo']['publishedDate']
+  @description = params['book']['volumeInfo']['description']
+  @pages = params['book']['volumeInfo']['pageCount']
+  @book_cover = params['book']['volumeInfo']['imageLinks']['thumbnail']
+  book = Book.create(title: @title, published_year: @published_year,description: @description, pages: @pages, book_cover: @book_cover, author: @author)
+  render json: book.to_json
   end
 
   def show
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,21 +1,6 @@
 class Book < ApplicationRecord
- has_one_attached :book_cover
  has_many :categories
- attr_accessor :title, :author, :published_year, :description, :pages, :book_cover
 
- def initialize(params_object)
-  p "Hello stop"
-  
-  @author = params_object['volumeInfo']['authors'].map { |i| i.to_s }.join(",")
-
-  @title = params_object['volumeInfo']['title']
-  @published_year = params_object['volumeInfo']['publishedDate']
-  @description = params_object['volumeInfo']['description']
-  @pages = params_object['volumeInfo']['pageCount']
-  @book_cover = params_object['volumeInfo']['imageLinks']['thumbnail']
-  # method to calculate genre here and choose which intelligence it acounts to.
-  
- end
 
 
  def process(snippet, description)
